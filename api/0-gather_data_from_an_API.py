c9bf608da3efce4e02b3893babcad98910f09c70
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-16 13:55:36 -0600

    many changes to try to match output

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,41 +1,62 @@
 #!/usr/bin/python3
-""" The Gather Data From An API Module """
+"""
+Gather Data From An API Module
+"""
+
 import requests
 import sys
 
+def get_user_info(employee_id):
+    """
+    Retrieve user information from the API.
+    """
+    api_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise requests.RequestException(f"Error: Unable to fetch user data. Status code: {response.status_code}")
 
 def get_employee_todo_progress(employee_id):
     """
     API endpoint
     """
-    api_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-
     try:
-        # Make a GET request to the API
-        response = requests.get(api_url)
+        # Retrieve user data to get the employee name
+        user_info = get_user_info(employee_id)
+        employee_name = user_info.get('name', f"User {employee_id}")
 
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
+        # Make a GET request to the API for TODO list
+        api_url_todos = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+        response_todos = requests.get(api_url_todos)
+
+        if response_todos.status_code == 200:
             # Convert the response to JSON format
-            todos = response.json()
+            todos = response_todos.json()
 
             # Filter completed tasks
             completed_tasks = [task for task in todos if task['completed']]
 
             # Display employee TODO list progress
-            print(f"Employee {todos[0]['userId']} is done with tasks ({len(completed_tasks)}/{len(todos)}):")
+            print(
+                f"Employee {employee_name} is done with tasks ({len(completed_tasks)}/{len(todos)}):")
 
             # Display titles of completed tasks
             for task in completed_tasks:
                 print(f"\t{task['title']}")
         else:
-            # Display an error message if the request was not successful
-            print(f"Error: Unable to fetch data. Status code:{response.status_code}")
+            # Display an error message if the request for TODO list was not successful
+            print(
+                f"Error: Unable to fetch TODO list data. Status code: {response_todos.status_code}")
 
-    except requests.ConnectionError:
-        # Display an error message for connection issues
-        print("Error: Unable to connect to the API.")
+    except requests.RequestException as e:
+        # Handle API request exceptions
+        print(f"Error: {e}")
 
+    except ValueError:
+        # Handle invalid employee ID
+        print("Error: Please provide a valid integer for the employee ID.")
 
 if __name__ == "__main__":
     """
