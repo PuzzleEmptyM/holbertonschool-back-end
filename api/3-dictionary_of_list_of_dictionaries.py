3663646be53b803f183fef8c5b8dee3fc899c606
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-21 13:57:45 -0600

    fixed ID issue

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -20,20 +20,22 @@ def get_all_employees_todo_progress():
     users_data = requests.get(users_url).json()
 
     for user in users_data:
-        employee_id = user["id"]
-        employee_name = user["username"]
+        employee_id = user.get("id")
+        if employee_id is not None:
+            employee_name = user["username"]
+            
+            todo_data = requests.get(todos_url,
+                                     params={"userId": employee_id}).json()
 
-        todo_data = requests.get(todos_url,
-                                 params={"userId": employee_id}).json()
-        tasks = []
-        for task in todo_data:
-            tasks.append({
-                "username": employee_name,
-                "task": task["title"],
-                "completed": task["completed"]
-            })
+            tasks = []
+            for task in todo_data:
+                tasks.append({
+                    "username": employee_name,
+                    "task": task["title"],
+                    "completed": task["completed"]
+                })
 
-        all_employees_data.append({str(employee_id): tasks})
+            all_employees_data.append({str(employee_id): tasks})
 
     json_file_path = "todo_all_employees.json"
 
